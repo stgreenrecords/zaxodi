93b9acb231dd7b6ee43d44e558db9a8a3407f781
Viachaslau Karnaushanka <stgreenrecords@gmail.com>
2016-10-25 21:45:37 +0300

    redesign homepage

--- portal-view/src/main/content/jcr_root/apps/portal/components/structure/topnav/clientlib/script.js
+++ portal-view/src/main/content/jcr_root/apps/portal/components/structure/topnav/clientlib/script.js
@@ -11,32 +11,30 @@ var PORTAL = (function (PORTAL, $) {
         var email = PORTAL.utils.get_cookie("portal-user");
 
         var isSessionIdValid = function(paramCookie, paramEmail){
-            $.ajax({
-                url: "/services/verifying",
-                type: "POST",
-                dataType: "json",
-                data: {
-                    'portal-session-id': paramCookie,
-                    'portal-user': paramEmail
-                },
-                success: function (data) {
-                    drawLastNavItem(data);
-                }
-            });
-        }
-
-        isSessionIdValid(sessionCookie,email);
-
-        var drawLastNavItem = function(data){
-            if ( data ){
-                var userNav = $self.find(".user_item");
-                userNav.css("display","block");
-                userNav.find(".basket_count").text(data.basketCount);
+            if (email && sessionCookie){
+                $.ajax({
+                    url: "/services/verifying",
+                    type: "POST",
+                    dataType: "json",
+                    data: {
+                        'portal-session-id': paramCookie,
+                        'portal-user': paramEmail
+                    },
+                    success: function (data) {
+                        if ( data ){
+                            var userNav = $self.find(".user_item");
+                            userNav.css("display","block");
+                            userNav.find(".basket_count").text(data.basketCount);
+                        }
+                    }
+                });
             } else {
                 $self.find(".login_home").css("display","block");
             }
+
         }
 
+        isSessionIdValid(sessionCookie,email);
     }
 
     return PORTAL;
